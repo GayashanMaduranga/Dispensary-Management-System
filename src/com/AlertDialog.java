4f1ac460a7047f11a17b0022c1d602b4f46a5e30
Dammakaru <dammakaru@gmail.com>
2017-08-01 17:00:42 +0530

    Changed Project Structure + Added JFoenix Library

--- src/com/AlertDialog.java
+++ src/com/AlertDialog.java
@@ -1,4 +1,4 @@
-package common;
+package com;
 
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
---
+++ src/com/patientmanagement/PatientManagement.java
@@ -0,0 +1,7 @@
+package com.patientmanagement;
+
+/**
+ * Created by Damma on 8/1/2017.
+ */
+public class PatientManagement {
+}
