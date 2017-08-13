bd7572ffe4dc21a143be6bb1096b043b3bb25a0f
Dammakaru <dammakaru@gmail.com>
2017-08-13 08:19:33 +0530

    connected Login to DB

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -2,23 +2,37 @@ package com.main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class Main extends Application {
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
         Parent root = FXMLLoader.load(getClass().getResource("./views/Login.fxml"));
         primaryStage.setTitle("Login");
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+        primaryStage.setX((screenBounds.getWidth() - primaryStage.getWidth()) / 2);
+        primaryStage.setY((screenBounds.getHeight() - primaryStage.getHeight()) / 2);
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
+
+
 }
