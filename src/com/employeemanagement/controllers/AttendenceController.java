5d833794afec2356d70277a55f24f6470656ccfb
gayashan kariyawasam <gmkariyawasam@gmail.com>
2017-08-14 12:38:23 +0530

    created new files

---
+++ src/com/employeemanagement/controllers/AttendenceController.java
@@ -0,0 +1,45 @@
+package com.employeemanagement.controllers;
+
+import com.jfoenix.controls.JFXButton;
+import com.test.ControlledScreen;
+import com.test.ScreenController;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by gayashan on 8/13/2017.
+ */
+public class AttendenceController implements Initializable,ControlledScreen {
+
+    ScreenController controller;
+
+    @FXML
+    private VBox aside;
+    @FXML
+    private JFXButton dashBoardBtn;
+
+    @Override
+    public void setScreenParent(ScreenController screenParent) {
+            controller = screenParent;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+    @FXML
+    void changeScene(MouseEvent event) {
+        if(dashBoardBtn == (JFXButton)event.getSource()){
+            controller.loadScreen(MyScreens.DASHBOARD_SCREEN,MyScreens.DASHBOARD_SCREEN_FXML );
+            controller.setScreen(MyScreens.DASHBOARD_SCREEN);
+            controller.unloadScreen(MyScreens.ADDEMPLOYEE_SCREEN);
+        }
+    }
+}
