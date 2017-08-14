dc8333a3abc9df619a9c5e67e0e3c47caf15f045
g_kariyawasam <gmkariyawasam@gmail.com>
2017-08-14 09:50:52 +0530

    creating a dash board and a testing stage

--- src/com/employeemanagement/controllers/DashBoardController.java
+++ src/com/employeemanagement/controllers/DashBoardController.java
@@ -1,7 +1,25 @@
 package com.employeemanagement.controllers;
 
+import com.test.ControlledScreen;
+import com.test.ScreenController;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by gayashan on 8/13/2017.
  */
-public class DashBoardController {
+public class DashBoardController implements Initializable,ControlledScreen {
+
+
+    @Override
+    public void setScreenParent(ScreenController screenParent) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
