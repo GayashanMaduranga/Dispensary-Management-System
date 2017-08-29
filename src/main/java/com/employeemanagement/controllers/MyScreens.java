b385472711e5d2961b25d7b522b04661301e7a50
gayashan kariyawasam <gmkariyawasam@gmail.com>
2017-08-29 14:41:34 +0530

    Bug Fixed in EmployeeManagement Module

--- src/main/java/com/employeemanagement/controllers/MyScreens.java
+++ src/main/java/com/employeemanagement/controllers/MyScreens.java
@@ -7,12 +7,12 @@ import com.common.BaseEnum;
  */
 public enum MyScreens implements BaseEnum{
 
-    DASHBOARD_SCREEN("dashboard", "/employeemanagement/DashBoard.fxml"),
-    ADDEMPLOYEE_SCREEN("addEmployee", "/employeemanagement/AddEmployee.fxml"),
-    ATTENDENCE_SCREEN("attendence", "/employeemanagement/Attendence.fxml"),
-    UPDATEADDEMPLOYEE_SCREEN("updateEmployee", "/employeemanagement/UpdateEmployee.fxml"),
-    PAYROLL_SCREEN("payroll", "/employeemanagement/Payroll.fxml"),
-    LOAN_SCREEN("loan", "/employeemanagement/Loan.fxml");
+    DASHBOARD_SCREEN("dashboard", "/com/employeemanagement/DashBoard.fxml"),
+    ADDEMPLOYEE_SCREEN("addEmployee", "/com/employeemanagement/AddEmployee.fxml"),
+    ATTENDENCE_SCREEN("attendence", "/com/employeemanagement/Attendence.fxml"),
+    UPDATEADDEMPLOYEE_SCREEN("updateEmployee", "/com/employeemanagement/UpdateEmployee.fxml"),
+    PAYROLL_SCREEN("payroll", "/com/employeemanagement/Payroll.fxml"),
+    LOAN_SCREEN("loan", "/com/employeemanagement/Loan.fxml");
 ///com/employeemanagement/views/Loan.fxml
 
     String path;
