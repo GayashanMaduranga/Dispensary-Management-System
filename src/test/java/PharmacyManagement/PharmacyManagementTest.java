6e1d10d6aaab6a80676125af7016dfc27a165092
gayashan kariyawasam <gmkariyawasam@gmail.com>
2017-09-12 21:31:24 +0530

    can Add Pharmacy Item

--- src/test/java/PharmacyManagement/PharmacyManagementTest.java
+++ src/test/java/PharmacyManagement/PharmacyManagementTest.java
@@ -1,5 +1,6 @@
 package PharmacyManagement;
 
+import com.EntityClasses.PharmacyItem;
 import db.UserSession;
 import org.hibernate.Session;
 import org.junit.Test;
@@ -13,6 +14,15 @@ public class PharmacyManagementTest {
     public void canAddPharmacyItem(){
         Session session = UserSession.getSession();
 
+        PharmacyItem item = new PharmacyItem();
+        item.setItemName("Some Item");
+        item.setMRP(123);
+        item.setReorderLevel(12312313);
+        item.setStock(23234);
+
+        session.beginTransaction();
+        session.save(item);
+        session.getTransaction().commit();
 
 
     }
