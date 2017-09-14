30dbf5885195e45f343540739b063f8c6eb28723
gayashan kariyawasam <gmkariyawasam@gmail.com>
2017-09-14 16:46:58 +0530

    Changed EmployeeManagement Controllers according to new Changes

--- src/main/java/com/employeemanagement/controllers/LoanController.java
+++ src/main/java/com/employeemanagement/controllers/LoanController.java
@@ -18,21 +18,10 @@ import java.util.ResourceBundle;
 /**
  * Created by gayashan on 8/13/2017.
  */
-public class LoanController implements Initializable,ControlledScreen {
+public class LoanController implements Initializable {
 
-    ScreenController controller;
 
-    @FXML
-    private VBox aside;
-    @FXML
-    private JFXButton dashBoardBtn;
-    @FXML
-    private JFXButton logoutBtn;
 
-    @Override
-    public void setScreenParent(ScreenController screenParent) {
-            controller = screenParent;
-    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -40,45 +29,15 @@ public class LoanController implements Initializable,ControlledScreen {
     }
 
 
-    @FXML
-    void changeScene(MouseEvent event) {
-        switch (((JFXButton) event.getSource()).getId()){
-            case "dashBoardBtn":
-                ScreenController.changeScreen(controller, MyScreens.LOAN_SCREEN, MyScreens.DASHBOARD_SCREEN);
-                break;
-            case "addEmployeeBtn":
-                ScreenController.changeScreen(controller, MyScreens.LOAN_SCREEN, MyScreens.ADDEMPLOYEE_SCREEN);
-                break;
-            case "updateEmployeeBtn":
-                ScreenController.changeScreen(controller, MyScreens.LOAN_SCREEN, MyScreens.UPDATEADDEMPLOYEE_SCREEN);
-                break;
-            case "attendenceBtn":
-                ScreenController.changeScreen(controller,MyScreens.LOAN_SCREEN, MyScreens.ATTENDENCE_SCREEN);
-                break;
-            case "payrollBtn":
-                ScreenController.changeScreen(controller, MyScreens.LOAN_SCREEN, MyScreens.PAYROLL_SCREEN);
-                break;
-            case "reportsBtn":
-                //ScreenController.changeScreen(controller, SupplierScreens.ADDEMPLOYEE_SCREEN, SupplierScreens.R);
-                System.out.println("null");
-                break;
-        }
-    }
+
 
     @FXML
     void goBack(MouseEvent event) {
-        ScreenController.changeScreen(controller, MyScreens.LOAN_SCREEN, MyScreens.PAYROLL_SCREEN);
+//        ScreenController.changeScreen(controller, MyScreens.LOAN_SCREEN, MyScreens.PAYROLL_SCREEN);
     }
 
 
 
 
-    @FXML
-    void logout(ActionEvent event) {
-        if(ConfirmDialog.show("", "Are you sure you want to logout?")){
-            Main.createLogin(new Stage());
-            Stage s = (Stage)logoutBtn.getScene().getWindow();
-            s.close();
-        }
-    }
+
 }
