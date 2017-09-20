9febf52c5cf7fc32fdf1ccec166fc0c4d3c87d1d
naveenlukefernando <naveen.luke@my.sliit.lk>
2017-09-20 18:48:20 +0530

    commit naveen

---
+++ src/main/java/com/suppliermanagement/controllers/Inventory_CTRL/Inventory_View_CTRL.java
@@ -0,0 +1,7 @@
+package com.suppliermanagement.controllers.Inventory_CTRL;
+
+/**
+ * Created by Naveen Luke Fernando on 2017-09-20.
+ */
+public class Inventory_View_CTRL {
+}
---
+++ src/main/java/com/suppliermanagement/controllers/Inventory_View/Inventory_View_CTRL.java
@@ -0,0 +1,7 @@
+package com.suppliermanagement.controllers.Inventory_View;
+
+/**
+ * Created by Naveen Luke Fernando on 2017-09-20.
+ */
+public class Inventory_View_CTRL {
+}
